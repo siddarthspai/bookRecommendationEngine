a746c3b5f15ada13c85f77d82db86e7c59819f94
Nikhil713 <nikhilbabu213@gmail.com>
2019-11-27 10:16:00 +0530

    Load book image from db

--- static/landing/landing.php
+++ static/landing/landing.php
@@ -111,14 +111,39 @@
                 </script>
                 </div>
                 <div class="bookCase">
+                <!--  <div><img src="../../assets/book4.jpg"></div>
                     <div><img src="../../assets/book4.jpg"></div>
                     <div><img src="../../assets/book4.jpg"></div>
                     <div><img src="../../assets/book4.jpg"></div>
                     <div><img src="../../assets/book4.jpg"></div>
                     <div><img src="../../assets/book4.jpg"></div>
                     <div><img src="../../assets/book4.jpg"></div>
-                    <div><img src="../../assets/book4.jpg"></div>
-                    <div><img src="../../assets/book4.jpg"></div>
+                    <div><img src="../../assets/book4.jpg"></div>-->
+                
+                <?php
+                    $host ="localhost";
+                    $uname = "root";
+                    $pwd = 'root@123';
+                    $db_name = "ASD";
+
+                    $file_path = '../../assets';
+                    $result = mysqli_connect($host,$uname,$pwd,$db_name) or die("Could     not                           connect to database." .mysqli_error());
+                    mysqli_select_db($result,$db_name) or die("Could not select    the database." .mysqli_error());
+                    $image_query = mysqli_query($result,"select book_img  from user_book");
+                    while($rows = mysqli_fetch_array($image_query))
+                    {
+                        $img_src = $rows['book_img'];
+                ?>
+<div >
+     
+        <img src="<?php echo $img_src; ?>" alt="" />
+     <!--   <p><strong><?php echo $img_name; ?></strong></p>-->
+     
+        </div>
+
+        <?php
+        }
+    ?>
                 </div>
             </div>
             <div class="recommend">
